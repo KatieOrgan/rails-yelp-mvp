801375142652fffb24a0b968ddcd5935f37acb4d
katie organ <katievictoriaorgan@gmail.com>
2025-01-08 17:01:55 +0000

    validations

--- db/migrate/20250108155428_create_reviews.rb
+++ db/migrate/20250108155428_create_reviews.rb
@@ -3,7 +3,8 @@ class CreateReviews < ActiveRecord::Migration[7.1]
     create_table :reviews do |t|
       t.integer :rating
       t.string :content
-
+      t.references :restaurant, null: false, foreign_key: true
+      
       t.timestamps
     end
   end
